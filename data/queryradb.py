4ffedc65933fd5494cdeb2aceea82d64f4957319
Jeff Bartig <jbartig@dhcp-223-12.meetings.nanog.org>
2017-06-04 14:30:06 -0700

    added queryname

--- data/queryradb.py
+++ data/queryradb.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 
 import subprocess
+import re
 
-def queryradb(asn, queryhost='whois.radb.net'):
+def queryname(asn, queryhost='whois.radb.net'):
+	command = ['whois', '-h', queryhost, 'as{}'.format(asn)]
+
+	response = subprocess.check_output(command)
+	lines = response.split("\n")
+
+	nameline = filter(lambda line: re.match("^as-name:", line), lines)[0]
+
+	name = re.search("^as-name:\s+(.*)$", nameline).group(1)
+	
+	return(name)
+
+def queryprefixes(asn, queryhost='whois.radb.net'):
 	command = ['whois', '-h', queryhost, '!gas{}'.format(asn)]
 
-	print command
-	
 	response = subprocess.check_output(command)
 	prefixes = response.split("\n")[1].split(" ")
 	
 	return(prefixes)
 
 def main():
-	print(queryradb(2381,'whois.radb.net'))
+	name = queryname(2381)
+	prefixes = queryprefixes(2381,'whois.radb.net')
+
+	print("Name: ", name)
+	print("Prefixes: ", prefixes)
 
 
 if __name__ == "__main__":
